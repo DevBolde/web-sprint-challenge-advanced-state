e4d425f2ec9fb95808acc1fe36f7d9f4f4463d55
Devin Bolden <devinbolden6@gmail.com>
2023-05-01 16:21:50 -0500

    finished wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -1,19 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { moveClockwise, moveCounterClockwise } from '../state/action-creators';
 
 export default function Wheel(props) {
+  const dispatch = useDispatch();
+  const wheelIndex = useSelector(state => state.wheel);
+
+  const handleClickClockwise = () => {
+    dispatch(moveClockwise());
+  };
+
+  const handleClickCounterClockwise = () => {
+    dispatch(moveCounterClockwise());
+  };
+
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className={`cog ${wheelIndex === 0 ? 'active' : ''}`} style={{ "--i": 0 }}>
+  {wheelIndex === 0 && <span className="inner-cog">B</span>}
+</div>
+<div className={`cog ${wheelIndex === 1 ? 'active' : ''}`} style={{ "--i": 1 }}>
+  {wheelIndex === 1 && <span className="inner-cog">B</span>}
+</div>
+
+<div className={`cog ${wheelIndex === 2 ? 'active' : ''}`} style={{ "--i": 2 }}>
+  {wheelIndex === 2 && <span className="inner-cog">B</span>}
+</div>
+
+<div className={`cog ${wheelIndex === 3 ? 'active' : ''}`} style={{ "--i": 3 }}>
+  {wheelIndex === 3 && <span className="inner-cog">B</span>}
+</div>
+<div className={`cog ${wheelIndex === 4 ? 'active' : ''}`} style={{ "--i": 4 }}>
+  {wheelIndex === 4 && <span className="inner-cog">B</span>}
+</div>
+<div className={`cog ${wheelIndex === 5 ? 'active' : ''}`} style={{ "--i": 5 }}>
+  {wheelIndex === 5 && <span className="inner-cog">B</span>}
+</div>
       </div>
       <div id="keypad">
-        <button id="counterClockwiseBtn" >Counter clockwise</button>
-        <button id="clockwiseBtn">Clockwise</button>
+        <button id="counterClockwiseBtn" onClick={handleClickCounterClockwise}>Counter clockwise</button>
+        <button id="clockwiseBtn" onClick={handleClickClockwise}>Clockwise</button>
       </div>
     </div>
   )
